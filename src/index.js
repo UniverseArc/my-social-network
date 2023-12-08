232642e304c02db714263b41632144e495104ede
UniverseArc <93486544+UniverseArc@users.noreply.github.com>
2023-12-08 21:26:31 +0300

    all: fixed

--- src/index.js
+++ src/index.js
@@ -2,28 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import State, { addMessageInChat, addPostOnWall, seeTheActionsOfUserInTextarea, subscribe, watchForInputUserInTexteareOfChats } from './components/Redux/State';
+import store from './components/Redux/redux-store';
+import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const rerenderEntireTree = (State) => {
     root.render(
         <React.StrictMode>
-
-            <App TransferState={State} 
-            watchForInputUserInTexteareOfChats={watchForInputUserInTexteareOfChats}
-
-            addMessageInChat={addMessageInChat} 
-
-            addPostOnWall={addPostOnWall} 
-            
-            seeTheActionsOfUserInTextarea={seeTheActionsOfUserInTextarea} />
-
+            <Provider store={store}>
+                <App />
+            </Provider>
         </React.StrictMode>
     );
 }
+rerenderEntireTree(store.getState())
 
-rerenderEntireTree(State)
-subscribe(rerenderEntireTree);
+store.subscribe(() => {
+    let State = store.getState();
+    rerenderEntireTree (State)
+});
 
 
 
